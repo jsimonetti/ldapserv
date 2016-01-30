ca98e7d12996c4733078ce31da4a332b9338a00b
Jeroen Simonetti <jeroen@simonetti.nl>
2016-01-30 03:25:41 +0100

    Add better dumping
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- backend/debug/main.go
+++ backend/debug/main.go
@@ -12,84 +12,76 @@ type DebugBackend struct {
 	Log log.Logger
 }
 
-func (DebugBackend) Dump(obj interface{}) {
-	spew.Dump(obj)
-}
-
 func (d *DebugBackend) Add(w ldap.ResponseWriter, m *ldap.Message) {
 	r := m.GetAddRequest()
-	fmt.Printf("ADD %#v\n", r)
-	d.Dump(m)
+	spew.Dump(r)
 	res := ldap.NewAddResponse(ldap.LDAPResultOperationsError)
 	w.Write(res)
 }
 
 func (d *DebugBackend) Bind(w ldap.ResponseWriter, m *ldap.Message) {
 	r := m.GetBindRequest()
-	fmt.Printf("BIND %#v\n", r)
-	d.Dump(m)
+	spew.Dump(r)
 	res := ldap.NewBindResponse(ldap.LDAPResultUnwillingToPerform)
 	w.Write(res)
 }
 
 func (d *DebugBackend) Delete(w ldap.ResponseWriter, m *ldap.Message) {
 	r := m.GetDeleteRequest()
-	fmt.Printf("DELETE %#v\n", r)
-	d.Dump(m)
+	spew.Dump(r)
 	res := ldap.NewDeleteResponse(ldap.LDAPResultSuccess)
 	w.Write(res)
 }
 
 func (d *DebugBackend) ExtendedRequest(w ldap.ResponseWriter, m *ldap.Message) {
 	r := m.GetExtendedRequest()
-	fmt.Printf("EXTENDED %#v\n", r)
-	d.Dump(m)
+	spew.Dump(r)
 	res := ldap.NewExtendedResponse(ldap.LDAPResultSuccess)
 	w.Write(res)
 }
 
 func (d *DebugBackend) Modify(w ldap.ResponseWriter, m *ldap.Message) {
 	r := m.GetModifyRequest()
-	fmt.Printf("MODIFY dn=%s\n", r.Object())
-	d.Dump(m)
+	spew.Dump(r)
 	res := ldap.NewModifyResponse(ldap.LDAPResultSuccess)
 	w.Write(res)
 }
 
 func (d *DebugBackend) ModifyDN(w ldap.ResponseWriter, m *ldap.Message) {
+	r := m.GetModifyRequest()
 	fmt.Printf("MODIFYDN %#v\n", m)
-	d.Dump(m)
+	spew.Dump(r)
 	res := ldap.NewModifyResponse(ldap.LDAPResultSuccess)
 	w.Write(res)
 }
 
 func (d *DebugBackend) PasswordModify(w ldap.ResponseWriter, m *ldap.Message) {
+	r := m.GetModifyRequest()
 	fmt.Printf("PASSWORD MODIFY %#v\n", m)
-	d.Dump(m)
+	spew.Dump(r)
 	res := ldap.NewExtendedResponse(ldap.LDAPResultSuccess)
 	w.Write(res)
 }
 
 func (d *DebugBackend) Search(w ldap.ResponseWriter, m *ldap.Message) {
-	fmt.Printf("SEARCH %#v\n", m)
-	d.Dump(m)
+	r := m.GetSearchRequest()
+	spew.Dump(r)
 	res := ldap.NewSearchResultDoneResponse(ldap.LDAPResultSuccess)
 	w.Write(res)
 }
 
 func (d *DebugBackend) Whoami(w ldap.ResponseWriter, m *ldap.Message) {
-	fmt.Println("WHOAMI")
-	d.Dump(m)
+	r := m.GetExtendedRequest()
+	spew.Dump(r)
 	res := ldap.NewExtendedResponse(ldap.LDAPResultSuccess)
 	w.Write(res)
 }
 
 func (d *DebugBackend) Abandon(w ldap.ResponseWriter, m *ldap.Message) {
-	var req = m.GetAbandonRequest()
-	fmt.Println("ABANDON %#v\n", m)
-	d.Dump(m)
+	r := m.GetAbandonRequest()
+	spew.Dump(m)
 	// retreive the request to abandon, and send a abort signal to it
-	if requestToAbandon, ok := m.Client.GetMessageByID(int(req)); ok {
+	if requestToAbandon, ok := m.Client.GetMessageByID(int(r)); ok {
 		requestToAbandon.Abandon()
 	}
 }
@@ -97,22 +89,21 @@ func (d *DebugBackend) Abandon(w ldap.ResponseWriter, m *ldap.Message) {
 func (d *DebugBackend) Compare(w ldap.ResponseWriter, m *ldap.Message) {
 	//r := m.GetCompareRequest()
 	fmt.Println("COMPARE %#v\n", m)
-	d.Dump(m)
+	spew.Dump(m)
 	res := ldap.NewCompareResponse(ldap.LDAPResultCompareTrue)
 	w.Write(res)
 }
 
 func (d *DebugBackend) Extended(w ldap.ResponseWriter, m *ldap.Message) {
-	//r := m.GetExtendedRequest()
-	fmt.Println("EXTENDED %#v\n", m)
-	d.Dump(m)
+	r := m.GetExtendedRequest()
+	spew.Dump(r)
 	res := ldap.NewExtendedResponse(ldap.LDAPResultSuccess)
 	w.Write(res)
 }
 
 func (d *DebugBackend) NotFound(w ldap.ResponseWriter, m *ldap.Message) {
 	fmt.Println("NotFound %#v\n", m)
-	d.Dump(m)
+	spew.Dump(m)
 	switch m.ProtocolOpType() {
 	case ldap.ApplicationBindRequest:
 		res := ldap.NewBindResponse(ldap.LDAPResultSuccess)
