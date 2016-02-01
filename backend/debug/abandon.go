a7757a943689cb4b21f65dd0241f0e2487db4b13
Jeroen Simonetti <jeroen@simonetti.nl>
2016-02-01 13:51:52 +0100

    Use util dump function
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- backend/debug/abandon.go
+++ backend/debug/abandon.go
@@ -1,13 +1,10 @@
 package debug
 
-import (
-	"github.com/davecgh/go-spew/spew"
-	"github.com/jsimonetti/ldapserv/ldap"
-)
+import "github.com/jsimonetti/ldapserv/ldap"
 
 func (d *DebugBackend) Abandon(w ldap.ResponseWriter, m *ldap.Message) {
 	r := m.GetAbandonRequest()
-	spew.Dump(r)
+	dump(r)
 	// retreive the request to abandon, and send a abort signal to it
 	if requestToAbandon, ok := m.Client.GetMessageByID(int(r)); ok {
 		requestToAbandon.Abandon()
